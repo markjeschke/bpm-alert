54c35cf7e41a4be4343ffd41c053c8ba4cd3fa51
Mark Jeschke <markjeschke@yahoo.com>
2025-05-03 18:03:47 -0700

    Initial Commit

---
+++ BPM Alert/BPM_AlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPM_AlertApp.swift
+//  BPM Alert
+//
+//  Created by Mark Jeschke on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BPM_AlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BPM Alert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BPM Alert
+//
+//  Created by Mark Jeschke on 5/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
